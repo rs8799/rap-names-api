ffac17fc99401b1ada562943cf1916a0b1deface
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-05-30 20:41:22 -0400

    working rapper api

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/api/:name',(request,response)=>{
     response.json(rappers['unknown'])
   }
 })
-app.listen(PORT,() => {
+app.listen(process.env.PORT || PORT,() => {
   console.log(`The server is now running on port ${PORT}! GET IT!`)
 })
